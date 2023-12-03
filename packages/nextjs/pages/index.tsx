9c1155c8789df8ef324fcef63f288da3512cb712
Jason Banks <jason@eras.fyi>
2023-12-03 15:15:56 +0100

    Add propose content input form to UI

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -10,6 +10,7 @@ import { hardhat } from "viem/chains";
 import { useAccount, useSignMessage } from "wagmi";
 // Wagmi hooks for wallet account management and message signing
 import { MetaHeader } from "~~/components/MetaHeader";
+import { InputBase } from "~~/components/scaffold-eth";
 // Custom component for the meta header
 import { Address } from "~~/components/scaffold-eth/Address";
 // Custom component to display blockchain addresses
@@ -42,7 +43,7 @@ const Home: NextPage = () => {
   const [question, setQuestion] = useState<string>("");
   const [answers, setAnswers] = useState<string[]>([]);
   const [contentItemHash, setContentItemHash] = useState<string>("");
-  const [transactionHash, setTransactionHash] = useState<string>("");
+  const [consumeTransactionHash, setConsumeTransactionHash] = useState<string>("");
   const [transactionSignature, setTransactionSignature] = useState<string>("");
   // Hook to sign a message (blockchain transaction) with the user's private key
   const { data: signedContentItemHash, signMessage } = useSignMessage({
@@ -50,6 +51,13 @@ const Home: NextPage = () => {
   });
   const [selectedAnswer, setSelectedAnswer] = useState<string>("");
   const [linkClicked, setLinkClicked] = useState<boolean>(false);
+
+  // Variables for proposing a new content item
+  const [userInputUrl, setUserInputUrl] = useState<string>("");
+  const [fetchedTitle, setFetchedTitle] = useState<string>("");
+  const [isTitleFetched, setIsTitleFetched] = useState<boolean>(false);
+  const [fetchTitleErrorMessage, setFetchTitleErrorMessage] = useState<string>("");
+  const [proposeTransactionHash, setProposeTransactionHash] = useState<string>("");
 
   // useEffect hooks are used to perform side effects in the component, like API calls, data fetching, etc.
   useEffect(() => {
@@ -113,19 +121,26 @@ const Home: NextPage = () => {
 
         console.log("userActionTransactionHash:", userActionTransactionHash);
 
-        setTransactionHash(userActionTransactionHash);
+        setConsumeTransactionHash(userActionTransactionHash);
       };
       sendUserActionTransaction();
     }
   }, [transactionSignature]);
 
   useEffect(() => {
-    if (transactionHash.length > 0) {
-      console.log("transactionHash has been updated:", transactionHash);
+    if (consumeTransactionHash.length > 0) {
+      console.log("transactionHash has been updated:", consumeTransactionHash);
       getContentItem();
       setLinkClicked(false);
     }
-  }, [transactionHash]);
+  }, [consumeTransactionHash]);
+
+  useEffect(() => {
+    if (fetchedTitle.length > 0) {
+      // do something
+      setProposeTransactionHash("");
+    }
+  }, [fetchedTitle]);
 
   // Function to handle link clicks
   const handleLinkClick = () => {
@@ -172,6 +187,55 @@ const Home: NextPage = () => {
     }
   };
 
+  // Function to handle URL form submission
+  const handleUrlSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsTitleFetched(false); // Reset the success flag
+    setFetchTitleErrorMessage(""); // Clear any previous error message
+
+    if (!isValidUrl(userInputUrl)) {
+      setFetchTitleErrorMessage("Please enter a valid URL.");
+      return;
+    }
+
+    try {
+      getURLTitle();
+    } catch (error) {
+      console.error("Error fetching title:", error);
+      setFetchTitleErrorMessage("An error occurred while fetching the title. Please try again.");
+    }
+  };
+
+  // Function to validate URL
+  const isValidUrl = (urlString: string): boolean => {
+    try {
+      // TODO: make URL validation more robust
+      new URL(urlString); // The URL constructor throws an error for invalid URLs
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  // Function to fetch the title of a URL from an API
+  const getURLTitle = () => {
+    // TODO: move API URL to .env file
+    fetch("http://localhost:50321/functions/v1/proposedContentTitleGetter", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // TODO: move bearer token to .env file
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0`,
+      },
+      body: JSON.stringify({ userAddress: address }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setFetchedTitle(data.title);
+        setIsTitleFetched(true); // Set success flag
+      });
+  };
+
   // The return statement of the component, which renders the UI
   // TODO: display a quiz/form with the questions and answers after the user has clicked the link
   //       when the user submits the correct answer, the userAction function should be called
@@ -189,7 +253,7 @@ const Home: NextPage = () => {
             <a href={url} target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>
               {title}
             </a>
-            {transactionHash.length > 0 && <div>Success! Transaction Hash: {transactionHash}</div>}
+            {consumeTransactionHash.length > 0 && <div>Success! Transaction Hash: {consumeTransactionHash}</div>}
           </div>
         )}
       </div>
@@ -215,16 +279,42 @@ const Home: NextPage = () => {
           </form>
         )}
       </div>
-      <div className="flex items-center flex-col flex-grow pt=10">Total Reads</div>
-      <div className="flex items-center flex-col flex-grow pt=10">
+      <div className="flex items-center flex-col flex-grow pt=10 my-10">
+        <h2>Total Reads</h2>
+        {/* </div>
+      <div className="flex items-center flex-col flex-grow pt=10"> */}
         <div className="p-4 text-4xl">{totalReadCount?.toString()}</div>
       </div>
-      <div className="flex items-center flex-col flex-grow pt=10">Your Contract</div>
-      <div className="flex items-center flex-col flex-grow pt=10">
+      <div className="flex items-center flex-col flex-grow pt=10 my-10">
+        <h2>Your Count</h2>
+        {/* </div>
+      <div className="flex items-center flex-col flex-grow pt=10"> */}
         <Address address={address} />
         <div className="p-4 text-4xl">{userReadCount?.toString()}</div>
       </div>
       <></>
+      {/* Form for proposing a new content item */}
+      <div className="flex items-center flex-col flex-grow pt=10 my-10">
+        <h2>Propose</h2>
+        <p>Propose financial literacy and well-being content to earn rewards!</p>
+        {/* </div>
+      <div className="flex items-center flex-col flex-grow pt=10"> */}
+        <form onSubmit={handleUrlSubmit} className="flex flex-col items-center">
+          <InputBase
+            name="url"
+            placeholder="URL"
+            value={userInputUrl}
+            onChange={setUserInputUrl}
+            error={fetchTitleErrorMessage.length > 0}
+          />
+          <button type="submit" className="btn btn-primary my-4">
+            Propose Content
+          </button>
+        </form>
+        {isTitleFetched && <p>Title from URL successfully obtained! Sending to the blockchain...</p>}
+        {proposeTransactionHash.length > 0 && <p>Successfully proposed! Transaction Hash: {proposeTransactionHash}</p>}
+        {fetchTitleErrorMessage && <p>{fetchTitleErrorMessage}</p>}
+      </div>
     </div>
   );
 };
